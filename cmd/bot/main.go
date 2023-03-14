0d2fd807a780a817b3ec9b38074b44494c8bd4eb
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-15 01:18:05 +0300

    method of getting .env info changed

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/shipaaa/telegram-sport-bot/pkg/config"
+	"github.com/joho/godotenv"
 	"github.com/shipaaa/telegram-sport-bot/pkg/telegram"
 
 	"log"
+	"os"
 )
 
 func main() {
-	cfg, err := config.Init()
-	if err != nil {
-		log.Fatal(err)
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
-	bot, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
 		log.Panic(err)
 	}
-	bot.Debug = true
+	bot.Debug = false
 
 	telegramBot := telegram.NewBot(bot)
 	if err := telegramBot.Start(); err != nil {
