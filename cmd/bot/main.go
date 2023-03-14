ce48b8c56ac524253b6a4f97f4e5f5e1c2c4d096
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-14 11:05:03 +0300

    bot's logic move to another package

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/shipaaa/telegram-sport-bot/pkg/config"
+	"github.com/shipaaa/telegram-sport-bot/pkg/telegram"
 
 	"log"
 )
@@ -12,28 +13,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	bot, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-		}
+	telegramBot := telegram.NewBot(bot)
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
 	}
 }
