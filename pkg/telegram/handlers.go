1263ed392906aef8cb9ce79e7c073daef580945e
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-14 13:11:48 +0300

    add bot commands

--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -5,14 +5,52 @@ import (
 	"log"
 )
 
+const (
+	startCommand = "start"
+	helpCommand  = "help"
+)
+
 func (b *Bot) handleUpdates(updates tgbotapi.UpdatesChannel) {
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
+		if update.Message.IsCommand() {
+			b.handleCommand(update.Message)
+			continue
+		}
 
 		b.handleMessage(update.Message)
 	}
+}
+
+func (b *Bot) handleCommand(message *tgbotapi.Message) error {
+	switch message.Command() {
+	case startCommand:
+		return b.handleStartCommand(message)
+	case helpCommand:
+		return b.handleHelpCommand(message)
+	default:
+		return b.handleUnknownCommand(message)
+	}
+}
+
+func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Привет. Ты ввел команду /start")
+	_, err := b.bot.Send(msg)
+	return err
+}
+
+func (b *Bot) handleHelpCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "А это команда /help")
+	_, err := b.bot.Send(msg)
+	return err
+}
+
+func (b *Bot) handleUnknownCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Я не знаю такой команды")
+	_, err := b.bot.Send(msg)
+	return err
 }
 
 func (b *Bot) handleMessage(message *tgbotapi.Message) {
