0a80678f3b18f02da042632250f6e68f3e52d780
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-16 00:59:25 +0300

    added exercise output before sending data

--- pkg/telegram/keyboards.go
+++ pkg/telegram/keyboards.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/shipaaa/telegram-sport-bot/pkg/models"
+	"time"
 )
 
 func (b *Bot) keyboardAllExercises() tgbotapi.InlineKeyboardMarkup {
@@ -28,15 +29,31 @@ func (b *Bot) callbackAllExercisesKeyboard(upd tgbotapi.Update) {
 		panic(err)
 	}
 
+	var exercise string
+	for _, internalSlice := range upd.CallbackQuery.Message.ReplyMarkup.InlineKeyboard {
+		for index := range internalSlice {
+			if *internalSlice[index].CallbackData == upd.CallbackQuery.Data {
+				exercise = internalSlice[index].Text
+				if exercise == "Спина" {
+					exercise = "Спину" // Подумать как исправить хардкодинг
+				}
+			}
+		}
+	}
+
+	chatId := upd.CallbackQuery.Message.Chat.ID
+	exerciseMessage := fmt.Sprintf("Присылаю упражнения на <b>%s</b>\nСекундочку...", exercise)
+	b.bot.Send(tgbotapi.MessageConfig{BaseChat: tgbotapi.BaseChat{ChatID: chatId},
+		ParseMode: "HTML", Text: exerciseMessage})
+	time.Sleep(2 * time.Second)
+
 	for _, d := range models.ConnectToDataBase(callback.Text) {
 		textInformation := fmt.Sprintf("<b>%s</b>\n\n%s\n\n%s", d.Exercise, d.Description, d.Reference)
-		chatId := upd.CallbackQuery.Message.Chat.ID
 
 		msg := tgbotapi.MessageConfig{BaseChat: tgbotapi.BaseChat{ChatID: chatId}, ParseMode: "HTML",
 			DisableWebPagePreview: true, Text: textInformation}
-
 		if _, err := b.bot.Send(msg); err != nil {
-			b.bot.Send(tgbotapi.NewMessage(upd.CallbackQuery.Message.Chat.ID, "Произошла ошибка, уже исправляем"))
+			b.bot.Send(tgbotapi.NewMessage(chatId, "Произошла ошибка, уже исправляем"))
 			panic(err)
 		}
 	}
