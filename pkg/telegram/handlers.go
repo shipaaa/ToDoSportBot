e2870d49343c4a1e6101dcd1f56ce58f3e37d3d8
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-16 23:33:41 +0300

    functions are shortened; added switch/case for callback

--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -1,6 +1,7 @@
 package telegram
 
 import (
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 )
@@ -9,21 +10,32 @@ const (
 	startCommand        = "start"
 	helpCommand         = "help"
 	allExercisesCommand = "allexercises"
+	trainingCommand     = "training"
 )
 
 func (b *Bot) handleUpdates(updates tgbotapi.UpdatesChannel) {
 	for update := range updates {
 		if update.Message != nil {
-
 			if update.Message.IsCommand() {
-				b.handleCommand(update.Message)
+				if err := b.handleCommand(update.Message); err != nil {
+					return
+				}
 				continue
 			}
 
-			b.handleMessage(update.Message)
+			if err := b.handleMessage(update.Message); err != nil {
+				return
+			}
 
 		} else if update.CallbackQuery != nil {
-			b.callbackAllExercisesKeyboard(update)
+			callbackQuery := b.getCallbackFromKeyboard(update)
+			switch callbackQuery.Data {
+			case "breast", "biceps", "triceps", "leg", "back", "shoulder":
+				chatId, exercise := b.sendWaitingMessage(callbackQuery.Message.ReplyMarkup, callbackQuery)
+				b.sendMessageForExerciseKeyboard(chatId, exercise)
+			case "day1", "day2", "day3":
+				// something new :)
+			}
 		}
 	}
 }
@@ -36,45 +48,36 @@ func (b *Bot) handleCommand(message *tgbotapi.Message) error {
 		return b.handleHelpCommand(message)
 	case allExercisesCommand:
 		return b.handleAllExercisesCommand(message)
+	case trainingCommand:
+		return b.handleTrainingProgram(message)
 	default:
 		return b.handleUnknownCommand(message)
 	}
 }
 
 func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
-	msg := tgbotapi.NewMessage(message.Chat.ID, "Привет. Ты ввел команду /start")
-	_, err := b.bot.Send(msg)
-	return err
+	return b.sendMessage(message.Chat.ID, "Привет. Ты ввел команду /start")
 }
 
 func (b *Bot) handleHelpCommand(message *tgbotapi.Message) error {
-	msg := tgbotapi.NewMessage(message.Chat.ID, "А это команда /help")
-	_, err := b.bot.Send(msg)
-	return err
+	return b.sendMessage(message.Chat.ID, "А это команда /help")
 }
 
 func (b *Bot) handleAllExercisesCommand(message *tgbotapi.Message) error {
-	msg := tgbotapi.NewMessage(message.Chat.ID, "Выбери какую группу мышц ты хочешь прокачать")
+	return b.sendKeyboard(message.Chat.ID, "Выбери какую группу мышц ты хочешь прокачать", keyboardAllExercises)
+}
 
-	msg.ReplyMarkup = b.keyboardAllExercises()
-
-	if _, err := b.bot.Send(msg); err != nil {
-		return err
-	}
-	return nil
+func (b *Bot) handleTrainingProgram(message *tgbotapi.Message) error {
+	//return b.sendMessage(message.Chat.ID, "Пока что не работает. Простите :(")
+	return b.sendKeyboard(message.Chat.ID, "Выбери день", keyboardTrainingProgram)
 }
 
 func (b *Bot) handleUnknownCommand(message *tgbotapi.Message) error {
-	msg := tgbotapi.NewMessage(message.Chat.ID, "Я не знаю такой команды")
-	_, err := b.bot.Send(msg)
-	return err
+	return b.sendMessage(message.Chat.ID, "Я не знаю такой команды")
 }
 
-func (b *Bot) handleMessage(message *tgbotapi.Message) {
+func (b *Bot) handleMessage(message *tgbotapi.Message) error {
 	log.Printf("[%s] %s", message.From.UserName, message.Text)
-
-	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
-	msg.ReplyToMessageID = message.MessageID
-
-	b.bot.Send(msg)
+	return b.sendMessage(message.Chat.ID, fmt.Sprintf("Я еще не настолько умный бот, поэтому общаюсь только"+
+		" командами.\nТыкай:\n\n/%s\n/%s\n/%s\n/%s", startCommand, helpCommand, allExercisesCommand, trainingCommand))
 }
