e7dbd7247d7efbfdde8851c2882d00103961b3f9
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-16 23:38:09 +0300

    callback method is divided into other functions and moved to other files; created 2nd keyboard func

--- pkg/telegram/keyboards.go
+++ pkg/telegram/keyboards.go
@@ -1,13 +1,10 @@
 package telegram
 
 import (
-	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/shipaaa/telegram-sport-bot/pkg/models"
-	"time"
 )
 
-func (b *Bot) keyboardAllExercises() tgbotapi.InlineKeyboardMarkup {
+func keyboardAllExercises() tgbotapi.InlineKeyboardMarkup {
 	keyboard := tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
 			tgbotapi.NewInlineKeyboardButtonData("Грудь", "breast"),
@@ -23,38 +20,22 @@ func (b *Bot) keyboardAllExercises() tgbotapi.InlineKeyboardMarkup {
 	return keyboard
 }
 
-func (b *Bot) callbackAllExercisesKeyboard(upd tgbotapi.Update) {
+func keyboardTrainingProgram() tgbotapi.InlineKeyboardMarkup {
+	keyboard := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("День 1", "day1"),
+			tgbotapi.NewInlineKeyboardButtonData("День 2", "day2"),
+			tgbotapi.NewInlineKeyboardButtonData("День 3", "day3"),
+		),
+	)
+	return keyboard
+}
+
+func (b *Bot) getCallbackFromKeyboard(upd tgbotapi.Update) *tgbotapi.CallbackQuery {
 	callback := tgbotapi.NewCallback(upd.CallbackQuery.ID, upd.CallbackQuery.Data)
-	if _, err := b.bot.Request(callback); err != nil {
+	if _, err := b.api.Request(callback); err != nil {
 		panic(err)
 	}
 
-	var exercise string
-	for _, internalSlice := range upd.CallbackQuery.Message.ReplyMarkup.InlineKeyboard {
-		for index := range internalSlice {
-			if *internalSlice[index].CallbackData == upd.CallbackQuery.Data {
-				exercise = internalSlice[index].Text
-				if exercise == "Спина" {
-					exercise = "Спину" // Подумать как исправить хардкодинг
-				}
-			}
-		}
-	}
-
-	chatId := upd.CallbackQuery.Message.Chat.ID
-	exerciseMessage := fmt.Sprintf("Присылаю упражнения на <b>%s</b>\nСекундочку...", exercise)
-	b.bot.Send(tgbotapi.MessageConfig{BaseChat: tgbotapi.BaseChat{ChatID: chatId},
-		ParseMode: "HTML", Text: exerciseMessage})
-	time.Sleep(2 * time.Second)
-
-	for _, d := range models.ConnectToDataBase(callback.Text) {
-		textInformation := fmt.Sprintf("<b>%s</b>\n\n%s\n\n%s", d.Exercise, d.Description, d.Reference)
-
-		msg := tgbotapi.MessageConfig{BaseChat: tgbotapi.BaseChat{ChatID: chatId}, ParseMode: "HTML",
-			DisableWebPagePreview: true, Text: textInformation}
-		if _, err := b.bot.Send(msg); err != nil {
-			b.bot.Send(tgbotapi.NewMessage(chatId, "Произошла ошибка, уже исправляем"))
-			panic(err)
-		}
-	}
+	return upd.CallbackQuery
 }
