a6bc067c72f8694c58f857f2a78acd9e5da106c8
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-16 19:00:56 +0300

    db added to main func

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
+	"github.com/shipaaa/telegram-sport-bot/pkg/models"
 	"github.com/shipaaa/telegram-sport-bot/pkg/telegram"
 
 	"log"
@@ -14,14 +15,19 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	botAPI, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
 		log.Panic(err)
 	}
-	bot.Debug = false
 
-	telegramBot := telegram.NewBot(bot)
-	if err := telegramBot.Start(); err != nil {
+	db, err := models.InitDB(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	bot := telegram.NewBot(botAPI, db)
+	if err := bot.Start(); err != nil {
 		log.Fatal(err)
 	}
 }
