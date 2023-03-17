cfcc7d27141b0531e0a70c2ed31ecc375753a41d
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-03-17 18:23:16 +0300

    sql file split into 2 files

---
+++ pkg/models/tablesCreation.sql
@@ -0,0 +1,41 @@
+CREATE TABLE breast (
+  id SERIAL,
+  exercise VARCHAR(50),
+  description TEXT,
+  reference TEXT
+);
+
+CREATE TABLE triceps (
+  id SERIAL,
+  exercise VARCHAR(50),
+  description TEXT,
+  reference TEXT
+);
+
+CREATE TABLE back (
+  id SERIAL,
+  exercise VARCHAR(50),
+  description TEXT,
+  reference TEXT
+);
+
+CREATE TABLE biceps (
+  id SERIAL,
+  exercise VARCHAR(50),
+  description TEXT,
+  reference TEXT
+);
+
+CREATE TABLE leg (
+  id SERIAL,
+  exercise VARCHAR(50),
+  description TEXT,
+  reference TEXT
+);
+
+CREATE TABLE shoulder (
+  id SERIAL,
+  exercise VARCHAR(50),
+  description TEXT,
+  reference TEXT
+);
