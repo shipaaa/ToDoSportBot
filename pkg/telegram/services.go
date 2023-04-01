0ca9dc1af06c45ef28cbde09c6037fa5a44a696e
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-04-01 08:47:11 +0300

    refactored

--- pkg/telegram/services.go
+++ pkg/telegram/services.go
@@ -8,27 +8,45 @@ import (
 )
 
 func getExerciseName(k *tgbotapi.InlineKeyboardMarkup, cbq *tgbotapi.CallbackQuery) string {
-	var exercise string
-	for _, v := range k.InlineKeyboard {
-		for i := range v {
-			if *v[i].CallbackData == cbq.Data {
-				exercise = v[i].Text
-				if exercise == "Спина" {
-					exercise = "Спину" // Подумать как исправить хардкодинг
-				}
+	button := getSelectedButton(k, cbq)
+	if button != nil {
+		exercise := button.Text
+		exercise = fixName(exercise)
+		return exercise
+	}
+	return ""
+}
+
+func getSelectedButton(k *tgbotapi.InlineKeyboardMarkup, cbq *tgbotapi.CallbackQuery) *tgbotapi.InlineKeyboardButton {
+	for _, row := range k.InlineKeyboard {
+		for _, button := range row {
+			if button.CallbackData != nil && *button.CallbackData == cbq.Data {
+				return &button
 			}
 		}
+	}
+	return nil
+}
+
+func fixName(exercise string) string {
+	switch exercise {
+	case "Спина":
+		exercise = "Спину"
 	}
 	return exercise
 }
 
 func getMessageTextForExerciseKeyboard(table []models.Table) string {
-	var sl []string
+	var rows []string
 	for _, t := range table {
-		dbRow := fmt.Sprintf("<b>%d. %s</b>\n\n%s\n\n<a href=\"%s\">Видосик обучалка</a>",
-			t.Id, t.Exercise, t.Description, t.Reference)
-		sl = append(sl, dbRow)
+		row := getTableRowText(t)
+		rows = append(rows, row)
 	}
-	messageText := strings.Join(sl, "\n\n")
-	return messageText
+	return strings.Join(rows, "\n\n")
+}
+
+func getTableRowText(t models.Table) string {
+	row := fmt.Sprintf("<b>%d. %s</b>\n\n%s\n\n<a href=\"%s\">Видосик обучалка</a>",
+		t.Id, t.Exercise, t.Description, t.Reference)
+	return row
 }
