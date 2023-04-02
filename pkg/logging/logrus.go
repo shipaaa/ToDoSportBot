f03140d658ff71eb37a3f87368d9c83377938cb2
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-04-02 13:06:56 +0300

    upd: log output to the console and file

--- pkg/logging/logrus.go
+++ pkg/logging/logrus.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"os"
 )
 
@@ -14,11 +15,8 @@ func StartLogging() {
 
 	log.SetFormatter(&log.TextFormatter{FullTimestamp: true, TimestampFormat: "02.01 15:04:05"})
 
-	// Output to stdout instead of the default stderr
-	//log.SetOutput(os.Stdout)
-
-	//Output to logfile
-	log.SetOutput(logFile)
+	// Output to log file and console
+	log.SetOutput(io.MultiWriter(logFile, os.Stdout))
 
 	//logrus show line number
 	log.SetReportCaller(true)
