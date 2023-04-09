73f8e32a14af5797ec31269ff35dc9645ca71112
Daniil Shapovalov <shhpprimeemployee@gmail.com>
2023-04-09 22:58:43 +0300

    upd: some changes

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,47 +16,30 @@ func Init() (*Config, error) {
 	}
 
 	var cfg Config
-	if err := unmarshal(&cfg); err != nil {
+	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
 	}
 
-	if err := fromEnv(&cfg); err != nil {
+	if err := loadEnv(&cfg); err != nil {
 		return nil, err
 	}
 
 	return &cfg, nil
 }
 
-func unmarshal(cfg *Config) error {
-	if err := viper.Unmarshal(&cfg); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func fromEnv(cfg *Config) error {
+func loadEnv(cfg *Config) error {
 	if err := viper.BindEnv("TELEGRAM_BOT_TOKEN"); err != nil {
 		return err
 	}
 	cfg.TelegramBotToken = viper.GetString("TELEGRAM_BOT_TOKEN")
 
-	if err := viper.BindEnv("DB_USER"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("DB_PASSWORD"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("DB_HOST"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("DB_NAME"); err != nil {
-		return err
-	}
-
+	dbUser := viper.GetString("DB_USER")
+	dbPassword := viper.GetString("DB_PASSWORD")
+	dbHost := viper.GetString("DB_HOST")
+	dbName := viper.GetString("DB_NAME")
 	cfg.DatabaseURL = fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
-		viper.GetString("DB_USER"), viper.GetString("DB_PASSWORD"), viper.GetString("DB_HOST"),
-		viper.GetString("DB_NAME"))
+		dbUser, dbPassword, dbHost, dbName)
+
 	return nil
 }
 
